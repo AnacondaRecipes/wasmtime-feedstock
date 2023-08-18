67c649e919009310ab83e933c8368ed0137fa335
Marek Waszkiewicz <mwaszkiewicz@anaconda.com>
2023-08-18 16:50:08 +0200

    address review comments

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,11 +6,6 @@ mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/lib
 
 cp ${SRC_DIR}/target/release/wasmtime ${PREFIX}/bin/
-
-if [ `uname` == Darwin ]; then  
-    cp ${SRC_DIR}/target/release/libwasmtime.dylib ${PREFIX}/lib/
-elif [ `uname` == Linux ]; then
-    cp ${SRC_DIR}/target/release/libwasmtime.so ${PREFIX}/lib/
-fi
+cp ${SRC_DIR}/target/release/libwasmtime$SHLIB_EXT ${PREFIX}/lib/
 cp ${SRC_DIR}/target/release/libwasmtime_cli.rlib ${PREFIX}/lib/
 
