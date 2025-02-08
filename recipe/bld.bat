8be286230c995c0d9a5e9664a130012d9bbd439a
ViridianMelody <mlove@anaconda.com>
2025-02-07 18:12:47 -0700

    Set CMAKE_GENERATOR on win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,11 @@
 cargo build --release
 
 echo "Building C libraries..."
-cmake -S crates\c-api -B target\c-api --install-prefix "%SRC_DIR%\artifacts"
+cmake -G"%CMAKE_GENERATOR%" -S crates\c-api -B target\c-api --install-prefix "%SRC_DIR%\artifacts"
 cmake --build target\c-api
 cmake --install target\c-api
 
 copy %SRC_DIR%\target\release\wasmtime.exe %LIBRARY_BIN%
 copy %SRC_DIR%\target\release\libwasmtime_cli.rlib  %LIBRARY_LIB%
 copy %SRC_DIR%\artifacts\lib\wasmtime.dll  %LIBRARY_BIN%
-copy %SRC_DIR%\artifacts\lib\wasmtime.dll.lib  %LIBRARY_LIB%
+copy %SRC_DIR%\artifacts\lib\wasmtime.dll.a  %LIBRARY_LIB%
