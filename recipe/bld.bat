bde69191fc672418890a618dce9bce951d57e79f
ViridianMelody <mlove@anaconda.com>
2025-02-10 13:04:16 -0700

    Adjust cmake windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,10 +1,8 @@
 cargo build --release
 
-set CMAKE_GENERATOR="Visual Studio 16 2019"
-
-cmake -G"%CMAKE_GENERATOR%" -S crates/c-api -B target/c-api --install-prefix "%SRC_DIR%/artifacts"
-cmake -G"%CMAKE_GENERATOR%" --build target/c-api
-cmake -G"%CMAKE_GENERATOR%" --install target/c-api
+cmake -G "Visual Studio 16 2019" -S crates/c-api -B target/c-api --install-prefix "%SRC_DIR%/artifacts"
+cmake --build target/c-api
+cmake --install target/c-api
 
 copy %SRC_DIR%\target\release\wasmtime.exe %LIBRARY_BIN%
 copy %SRC_DIR%\target\release\libwasmtime_cli.rlib  %LIBRARY_LIB%
